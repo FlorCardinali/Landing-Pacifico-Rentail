4109fae01e744352ef4625e05ee3c11a98134fd3
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2024-03-06 03:16:51 -0300

    correcion de posicion

--- Departamentos/src/components/hero/Hero.jsx
+++ Departamentos/src/components/hero/Hero.jsx
@@ -11,12 +11,12 @@ const Hero = () => {
         </div>
         
         <div className={styles.textContainer}>
-            <div>
+            <div className={styles.h1Container}>
                 <h1 className={styles.h1}>Pacifico</h1>
                 <h1  className={`${styles.h1} ${styles.secH1}`}>Rental</h1>
-                <p className={styles.subtitle}>Departamentos</p> 
+               
             </div>
-           
+            <p className={styles.subtitle}>Departamentos</p> 
             <div className={styles.buttonsContainer}>
                 <a className={styles.res}>
                     Reservas
