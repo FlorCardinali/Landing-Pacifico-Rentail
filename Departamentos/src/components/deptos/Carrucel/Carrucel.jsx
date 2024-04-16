5cad9ac82b1a37a9e403bed48dd9d7757af15023
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2024-04-16 17:38:55 -0300

    correccion y estilizacion del componente carrucel

--- Departamentos/src/components/deptos/Carrucel/Carrucel.jsx
+++ Departamentos/src/components/deptos/Carrucel/Carrucel.jsx
@@ -1,9 +1,9 @@
-import css from "./Carrucel.module.css"
-import { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import css from "./Carrucel.module.css";
 
 const Carrucel = ({images}) => {
-
     const containerRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const handleScroll = (scrollOffset) => {
         if (containerRef.current) {
@@ -11,28 +11,35 @@ const Carrucel = ({images}) => {
         }
     };
 
+    useEffect(() => {
+        // Al montar el componente, asegúrate de que el carrusel esté en la primera imagen
+        setCurrentIndex(0);
+        if (containerRef.current) {
+            containerRef.current.scrollTo({ left: 0, behavior: 'auto' });
+        }
+    }, []);
 
     return (  
-    <div className={css.container}>
-        <button className={`${css.btn} ${css.left}`} onClick={() => handleScroll(-100)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"/>
-            </svg>
-        </button>
+        <div className={css.generalContainer}>
+            <button className={`${css.btn} ${css.left}`} onClick={() => handleScroll(-100)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                    <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                </svg>
+            </button>
+            
+            <aside className={css.imgContainer} ref={containerRef}>
+                {images.map((img, index) => 
+                    <img key={index} className={css.img} src={img} alt="imagenes de los departamentos" />
+                )}
+            </aside>
         
-        <aside className={css.imgContainer} ref={containerRef}>
-            {images.map(
-                (img) => 
-                <img className={css.img} src={img} alt="" />
-            ) }
-        </aside>
-    
-        <button className={`${css.btn} ${css.right}`} onClick={() => handleScroll(100)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-right" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
-            </svg>
-        </button>
-    </div>
-     );
+            <button className={`${css.btn} ${css.right}`} onClick={() => handleScroll(100)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                </svg>
+            </button>
+        </div>
+    );
 }
+
 export default Carrucel;
--- Departamentos/src/components/deptos/Departamentos.jsx
+++ Departamentos/src/components/deptos/Departamentos.jsx
@@ -1,19 +1,23 @@
 import data from "../../data/dataDeptos.json"
+import css from "../deptos/Departamentos.module.css"
+import Carrucel from "./Carrucel/Carrucel";
 
 
 const Departamentos = () => {
 
     const depas = Object.values(data);
     return ( 
-        <section>
-            <div>
+        <section className={css.section}>
                {depas.map((depto,index) => 
+                <div key={Math.random()} className={css.deptoContainer}>
+                    <Carrucel images={depto.fotos}></Carrucel>
                     <p key={index} styles="color:red;">
-                        {depto.direccion}
-                        {depto.comodidades.cochera ? "pajaro ": "uwu"}
+                            {depto.direccion}
+                            {depto.comodidades.cochera ? "pajaro ": "uwu"}
                     </p>
-                )} 
-            </div>
+                    
+                </div>
+               )}      
         </section>
      );
 }
